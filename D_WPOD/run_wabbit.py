69f4faf92dd243a40f22505bd18ef7a4905ea8fa
Philipp Krah <philipp.krah@tu-berlin.de>
2019-05-18 12:10:19 +0200

    some scripts for wPOD evaluation

--- D_WPOD/run_wabbit.py
+++ D_WPOD/run_wabbit.py
@@ -14,38 +14,128 @@ The task is to study the dependence of the adaptation on the DECOMPOSITION.
 import re
 import os
 import glob
-
+import numpy as np
+from wPODdirs import *
 # %% Change parameters here:
-eps_list = [100, 10, 1, 0.1, 0.01, 0.001]
-wdir = "/home/phil/devel/WABBIT/"                                              # directory of wabbit-post 
-data = "/home/phil/devel/results/cyl_POD/wPOD/vor_crop_adapt/vorx_0000*.h5"  # data you want to use for POD
-mpicommand = "mpirun -np 4"                                                    # mpi command used for parallel execution
-memory ="--memory=8GB"                                                         # how much memory (RAM) available on your pc
-command = mpicommand + " " +  wdir + \
-         "wabbit-post --POD --save_all --nmodes=30 " + memory + \
-         " --adapt=%s " + data
+#eps_list = [100, 10, 1, 0.1, 0.01, 0.001]
+eps_list = [0, 0.001, 0.01]
 
-for eps in eps_list:
-    # -----------------------------
-    # Prepare to save data from POD
-    # -----------------------------
-    c = command % str(eps)   # change eps
-    c += " > wPOD.log" 
-    save_dir = "eps" + str(eps)
-    if not os.path.exists(save_dir):
-        os.mkdir(save_dir) # make directory for saving the files
-    os.chdir(save_dir)
+wdir = "/home/phil/devel/WABBIT/"                                              # directory of wabbit-post 
+data = "/home/phil/devel/results/cyl_POD/wPOD/vor_crop_adapt/vorx_0000*.h5"    # data you want to use for POD
+mpicommand = "mpirun -np 4"                                                    # mpi command used for parallel execution
+memory ="--memory=16GB"                                                         # how much memory (RAM) available on your pc
+
+
+
+# %% run wPOD for different eps:
+def run_wPOD_for_different_eps(wdir, data, eps_list, memory, mpicommand, save_log=False):
     
-    # -----------------------------
-    # Execute Command
-    # -----------------------------
-    print("\n\n###################################################################")
-    print("\t\teps =",eps)
-    print("###################################################################")
-    print("\n",c,"\n\n")
-    success = os.system(c)   # execute command
-    os.chdir("../")
-    if success != 0:
-        print("command did not execute successfully")
-        break
-    
+   
+    for eps in eps_list:
+        # ------------------------------
+        # BUILD EXECUTION COMMAND
+        # ------------------------------
+        if eps > 0: 
+            c = mpicommand + " " +  wdir + \
+             "wabbit-post --POD --save_all --nmodes=30 " + memory + \
+             " --adapt="+ str(eps) + " " + data
+             
+        else:
+            c = mpicommand + " " +  wdir + \
+             "wabbit-post --POD --save_all --nmodes=30 " + memory + \
+             " " + data
+        # pipe output into logfile     
+        if save_log:
+            c += " > wPOD.log" 
+        # -----------------------------
+        # Prepare to save data from POD
+        # -----------------------------
+        save_dir = "eps" + str(eps)
+        if not os.path.exists(save_dir):
+            os.mkdir(save_dir) # make directory for saving the files
+        os.chdir(save_dir)
+        
+        # -----------------------------
+        # Execute Command
+        # -----------------------------
+        print("\n\n###################################################################")
+        print("\t\teps =",eps)
+        print("###################################################################")
+        print("\n",c,"\n\n")
+        success = os.system(c)   # execute command
+        os.chdir("../")
+        if success != 0:
+            print("command did not execute successfully")
+            break
+    return success
+
+# %% reconstruct for different eps:
+def run_wPOD_reconstruction_for_different_eps(wdir, data, eps_list, memory, mpicommand):    
+    command = mpicommand + " " +  wdir + \
+             "wabbit-post --POD-reconstruct --save_all --nmodes=30 " + memory + \
+             " --adapt=%s " + data
+    for eps in eps_list:
+        # -----------------------------
+        # Prepare to save data from POD
+        # -----------------------------
+        c = command % str(eps)   # change eps
+        c += " > reconstruct.log" 
+        save_dir = "eps" + str(eps)
+        if not os.path.exists(save_dir):
+            os.mkdir(save_dir) # make directory for saving the files
+        os.chdir(save_dir)
+        
+        # -----------------------------
+        # Execute Command
+        # -----------------------------
+        print("\n\n###################################################################")
+        print("\t\teps =",eps)
+        print("###################################################################")
+        print("\n",c,"\n\n")
+        success = os.system(c)   # execute command
+        os.chdir("../")
+        if success != 0:
+            print("command did not execute successfully")
+            break
+        return success
+
+# %% adapt one snapshot for different eps: logfile written in adapt .log
+def adpatation_for_different_eps(wdir, h5_filename, eps_list, memory, mpicommand, create_log_file=True):
+    file = data_file.split("/")[-1]
+    command = mpicommand + " " +  wdir + \
+             "wabbit-post --dense-to-sparse --eps=%s " + memory + \
+              " "+ file
+              
+    for eps in eps_list:
+        # -----------------------------
+        # Prepare to save data from POD
+        # -----------------------------
+        c = command % str(eps)   # change eps
+        if create_log_file:
+            c += " > adapt.log" 
+        save_dir = "eps" + str(eps)
+        if not os.path.exists(save_dir):
+            os.mkdir(save_dir) # make directory for saving the files
+        os.chdir(save_dir)
+        # copy given file in current directory 
+        os.system("cp "+h5_filename+ " .")
+        # -----------------------------
+        # Execute Command
+        # -----------------------------
+        print("\n\n###################################################################")
+        print("\t\teps =",eps)
+        print("###################################################################")
+        print("\n",c,"\n\n")
+        success = os.system(c)   # execute command
+        os.chdir("../")
+        if success != 0:
+            print("command did not execute successfully")
+            break
+    return success
+
+# %% run scripts:
+run_wPOD_for_different_eps(wdir, data, eps_list, memory, mpicommand, save_log=True)
+#run_wPOD_for_different_eps(wdir, data, eps_list, save_dir, memory, mpicommand)
+#run_wPOD_reconstruction_for_different_eps(wdir, data, eps_list, memory, mpicommand)
+#data_file = np.sort(glob.glob(data))[100]
+#adpatation_for_different_eps(wdir, data_file, eps_list, memory, mpicommand)
--- D_WPOD/wPODdirs.py
+++ D_WPOD/wPODdirs.py
@@ -4,12 +4,20 @@
 Created on Sat Apr 20 18:13:54 2019
 
 @author: phil
-"""
 
-pic_dir = "./images/"
-#wdir = "/home/krah/develop/WABBIT/"
+PLEASE CHANGE ALL Directories here:
+
+"""
+###############################################################################
+# 1.) Location of images saved during post processing
+pic_dir = "./images/"           
+# 2.) directoy of wabbit-post executable
+#wdir = "/home/krah/develop/WABBIT/"++
 wdir = "~/savart/develop/WABBIT/"
+# 3.) results to compare data to
 resdir_flusi="../results/cyl_POD/wPOD/vor_crop/"
 resdir_flusi_modes="../results/cyl_POD/vorticity_POD2/modes/"
-resdir_wPOD_modes="~/savart/develop/WABBIT/19.04.19/"
+# 4.) Where should wabbit-post save its data
+resdir_wPOD_modes="/home/phil/develop/WABBIT/19.04.19/"
 resdir_wabbit =resdir_flusi + "_adapt/"
+###############################################################################
